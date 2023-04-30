38861338a745452733d736f7bb63671fa7466e64
김희찬 <caphile98@gmail.com>
2023-04-30 23:10:58 +0900

    O(n^2) 쉬운방법으로 완

--- 고급문제해결기법/순열 복원 (매운맛).py
+++ 고급문제해결기법/순열 복원 (매운맛).py
@@ -1,39 +1,15 @@
-class recover:
-    def __init__(self, n):
-        self.n = n
-        self.check = [0 for _ in range(n)]
-        self.bit = [0 for _ in range(n + 1)]
-        
-    def run(self):
-        self.prefixSum = []
-        for i in range(self.n):
-            self.prefixSum.append(self.preSum(i))
-            self.update(i, self.check[i])
+def reconstruct(B):
+    n = len(B)
+    A = [0 for _ in range(n)]
+    check = [i for i in range(n)]
+    for i in range(n - 1, -1, -1):
+        A[i] = check.pop(B[i])
+    return A
 
-    def update(self, i, val):
-        i += 1
-        while i < self.n + 1:
-            self.bit[i] += val
-            i += self.LSB(i)
+B = [int(x) for x in input().split()]
+A = reconstruct(B)
+print(A)
 
-    def preSum(self, i):
-        i += 1
-        s = 0
-        while i > 0:
-            s += self.bit[i]
-            i -= self.LSB(i)
-        return s
-
-    def LSB(self, i):
-        return i & -i
-
-B = list(map(int, input().split()))
-n = len(B)
-A = [-1 for _ in range(n)]
-
-rc = recover(n)
-rc.run()
-
-for i in range(n - 1, -1, -1):
-    rc.check[i] = 1
-    rc.run()
+# check 리스트에 리스트 A에 채워야 하는 값들을 저장하고 A를 채울때마다 해당 값을 check에서 pop O(n)
+# 항상 A의 채워지지 않은 마지막 인덱스부터 값을 채우면
+# check 리스트에서 남아있는 값들 중 B[i]번째의 값을 가지게 됨을 알 수 있음
