83c72697e546c258d372589bd049d29ca206945d
김희찬 <caphile98@gmail.com>
2023-05-12 00:08:39 +0900

    일부 런타임 오류(문의 필요)

---
+++ 고급문제해결기법/[트리-해보기1] 조상 확인하기.py
@@ -0,0 +1,47 @@
+class node:
+    def __init__(self, now):
+        self.n = now
+        self.c = []
+
+def preorder(now):
+    global ord
+    pre[now.n] = ord
+    ord += 1
+    for n in now.c:
+        preorder(nodes[n])
+
+def postorder(now):
+    global ord
+    for n in now.c:
+        postorder(nodes[n])
+    post[now.n] = ord
+    ord += 1
+
+n, q = map(int, input().split())
+
+nodes = [0]
+for i in range(1, n + 1):
+    nodes.append(node(i)) 
+
+for _ in range(n - 1):
+    p, c = map(int, input().split())
+    nodes[p].c.append(c)
+
+ord = 1
+pre = [0 for _ in range(n + 1)]
+preorder(nodes[1])
+
+ord = 1
+post = [0 for _ in range(n + 1)]
+postorder(nodes[1])
+
+count = 0
+for _ in range(q):
+    u, v = map(int, input().split())
+
+    if u == v:
+        count += 1
+    elif pre[u] < pre[v] and post[u] > post[v]:
+        count += 1
+
+print(count)
