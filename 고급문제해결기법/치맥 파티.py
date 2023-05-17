0c70204ec0de9d7c759859f47d0c6a3c645ec42c
김희찬 <caphile98@gmail.com>
2023-05-17 13:53:31 +0900

    치맥파티 완

---
+++ 고급문제해결기법/치맥 파티.py
@@ -0,0 +1,28 @@
+a, b, t = map(int, input().split())
+
+maxc = 0
+mint = 10000
+i = 0
+while 1:
+    temp = t - a * i
+    if temp < 0:
+        break
+    count = temp // b + i   # 먹은 치킨 수
+    time = temp % b         # 남은 시간
+
+    if mint > time:
+        mint = time
+        maxc = count
+    elif not time and maxc < count:
+        maxc = count
+    i += 1
+
+if not mint:
+    print(maxc)
+else:
+    print(maxc, mint)
+
+# 모든 가능한 (후라이드 수, 양념 )에 대해 먹은 치킨의 수, 남은 시간을 비교
+# 1. 이전에 저장된 시간보다 현재 시간이 줄었다면 시간과 먹은 수를 갱신
+# 2. 남은 시간이 0이라면 먹은 수만 갱신
+# 최대 a의 길이만큼만 반복하면 되므로 O(a) 혹은 O(b)이 가능함
