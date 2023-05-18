2dd9c8aa3b6f2d9c79e0c30651aa85cf8c599fac
김희찬 <caphile98@gmail.com>
2023-05-19 00:33:23 +0900

    line이 순환하지 않는다고 했을때 완료 23.05.19

--- 고급문제해결기법/쌍 무지개 뜨는 언덕.py
+++ 고급문제해결기법/쌍 무지개 뜨는 언덕.py
@@ -1,5 +1,5 @@
-def calc(opt):
-    global n, k
+def move(opt):
+    global n
     global nowCount, numCount
     global s, e
 
@@ -11,7 +11,7 @@ def calc(opt):
         if nums[line[s]] == 1:
             numCount += 1
         s += 1
-    else:
+    elif opt == 'e':
         e += 1
         nows[line[e]] += 1
         if nows[line[e]] == 1:
@@ -32,32 +32,45 @@ for i in range(n):
     c = int(input())
     nums[c] += 1
     line.append(c)
-line += line
 
-s, e = 0, 0
+s, e = 0, -1
+move('e')
 target = line[0]
 seq = False
 rainbows = []
-while s < n and e < 2 * n:  # 한 바퀴를 다 돌때까지
-    if e - s + 1 > n:
-        calc('s')
-        continue
+while s < n:  # 한 바퀴를 다 돌때까지
     if not nows[target]:
+        if e + 1 == n:
+            break
         seq = False
-        calc('e')
+        move('e')
     else:
-        if nowCount == k:   # 무지개   
+        if nowCount == k:       # 무지개   
             if numCount == k:   # 쌍무지개
                 if seq:
                     rainbows.pop()
                 rainbows.append(e - s + 1)
                 seq = True
-            calc('s')
+            move('s')
         else:
+            if e + 1 == n:
+                break
             seq = False
-            calc('e')
+            move('e')
 
-if rainbows == []:
+if not len(rainbows):
     print(0)
 else:
     print(min(rainbows))
+
+# 모든 무지개는 결국 모든 색을 가지고 있어야 하므로
+# 무지개는 최대 특정 색이 등장한 만큼까지만 만들기 가능 (코드에선 line[0]으로 지정)
+# 따라서 해당 색을 1개 이상 보유한 채로 무지개를 만들 수 있는 구간을 찾음
+# 1. 색이 없다면 e + 1
+# 2. 색이 있고 무지개가 만들어진다면
+# 2-1. 쌍무지개가 만들어진다면 무지개 길이 저장 후 s + 1
+# 이때 s + 1를 해서 똑같이 쌍무지개가 생길 수 있으므로 seq 상태 가져감
+# 2-2. 쌍무지개를 만들지 못한다면 e + 1
+# 저장된 쌍무지개가 있다면 그 중 최소값, 없다면 0을 출력
+# s와 e가 최대 n번까지만 탐색하므로 (move함수도 O(1))
+# O(n) 시간복잡도를 가짐
